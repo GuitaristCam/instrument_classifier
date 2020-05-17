0b4565beea731669fbea6dba7e8b6cf23e9079c6
Cameron Schettler <cameronschettler@gmail.com>
2020-05-17 16:24:40 -0400

    Tested the Commit feature in PyCharm, added a couple libraries to review/install in the TODO section

--- instrument_classifier/__init__.py
+++ instrument_classifier/__init__.py
@@ -1,1 +1,6 @@
 # todo http://ai.stanford.edu/~bangpeng/ppmi.html - people playing instruments dataset
+# todo http://www.pythonware.com/products/pil/ - Python Imaging Library
+# todo https://github.com/abidrahmank/OpenCV2-Python-Tutorials - OpenCV
+# todo http://simplecv.org/ - SimpleCV
+# todo https://mahotas.readthedocs.io/en/latest/ - Mahotas
+# todo https://pypi.org/project/pgmagick/ - pgmagick / GraphicsMagick
